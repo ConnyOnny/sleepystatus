aab7d7e1be70b04cc59ad89a7de11fed7db99975
Constantin Berhard <constantin@exxxtremesys.lu>
2015-02-10 13:37:53 +0100

    simplified battery_level

--- functions/battery_level.c
+++ functions/battery_level.c
@@ -26,24 +26,15 @@ int read_int_from_file(char const* fname) {
 
 void *battery_level (void *arg) {
 	size_t tid = (size_t)arg;
-	char buf[16];
 	// the prefix is there to indicate the max height of the bar
-	sprintf(buf,"▕");
-	size_t appendpos = strlen(buf);
-	char *levels[] = {"▁","▂","▃","▄","▅","▆","▇","█"};
+	char *levels[] = {"▕▁▏","▕▂▏","▕▃▏","▕▄▏","▕▅▏","▕▆▏","▕▇▏","▕█▏",""};
 	int lastidx=-1;
 	while (1) {
 		int bat_cur = read_int_from_file(ACPI_BATTERY_LEVEL_FILE);
 		int bat_max = read_int_from_file(ACPI_BATTERY_MAXLEVEL_FILE);
 		int idx = bat_cur * 8 / bat_max;
 		if (idx != lastidx) {
-			if (idx == 8) {
-				// battery 100% full, don't show anything
-				notify_update(tid,"");
-			} else {
-				sprintf(buf+appendpos,"%s",levels[idx]);
-				notify_update(tid,buf);
-			}
+			notify_update(tid,levels[idx]);
 		}
 		lastidx=idx;
 		system("sleep 5");
